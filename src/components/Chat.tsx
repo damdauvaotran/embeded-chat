2713f012a9d9af7e98c0847a56032dbf84df20f2
damdauvaotran <ngokhoaisan1999@gmail.com>
2023-08-14 01:20:49 +0700

    Add chat push logic

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -4,6 +4,7 @@ import { AiOutlineSend } from 'react-icons/ai';
 /** Local libs **/
 import style from './Chat.module.css';
 import Bubble from './Bubble';
+import { useState } from 'preact/hooks';
 
 /** Components **/
 
@@ -11,12 +12,55 @@ import Bubble from './Bubble';
 
 /** Interfaces, enum... **/
 export interface ChatProps {}
+interface IMessage {
+  avatar: string;
+  name: string;
+  time: string;
+  content: string;
+  isResponse?: boolean;
+  footer: string;
+}
 
 /** Variables **/
 
 /** ------------------------- **/
 const Chat = ({}) => {
   /** States **/
+  const [messages, setMessages] = useState<IMessage[]>([
+    {
+      avatar:
+        'https://avataaars.io/?avatarStyle=Circle&topType=LongHairBigHair&accessoriesType=Prescription02&hairColor=Platinum&facialHairType=BeardMajestic&facialHairColor=Red&clotheType=GraphicShirt&clotheColor=Heather&graphicType=Bear&eyeType=Surprised&eyebrowType=Default&mouthType=Sad&skinColor=Pale',
+      name: 'Obi-Wan Kenobi',
+      time: '12:45',
+      content: 'You were the Chosen One!',
+      isResponse: true,
+      footer: 'Delivered',
+    },
+    {
+      avatar:
+        'https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Kurt&facialHairType=BeardLight&facialHairColor=Blonde&clotheType=CollarSweater&clotheColor=PastelRed&eyeType=Cry&eyebrowType=UpDownNatural&mouthType=Disbelief&skinColor=Yellow',
+      name: 'Anakin',
+      content: 'I hate you!',
+      time: '12:46',
+      isResponse: false,
+      footer: 'Seen at 12:46',
+    },
+  ]);
+
+  const onChatSubmit = (messageContent: string) => {
+    setMessages([
+      {
+        avatar:
+          'https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Kurt&facialHairType=BeardLight&facialHairColor=Blonde&clotheType=CollarSweater&clotheColor=PastelRed&eyeType=Cry&eyebrowType=UpDownNatural&mouthType=Disbelief&skinColor=Yellow',
+        name: 'Anakin',
+        content: messageContent,
+        time: '12:46',
+        isResponse: false,
+        footer: 'Seen at 12:46',
+      },
+      ...messages,
+    ]);
+  };
 
   /** Hooks **/
 
@@ -28,30 +72,42 @@ const Chat = ({}) => {
 
   /** Elements **/
   return (
-    <div >
-      <Bubble
-        avatar="https://avataaars.io/?avatarStyle=Circle&topType=LongHairBigHair&accessoriesType=Prescription02&hairColor=Platinum&facialHairType=BeardMajestic&facialHairColor=Red&clotheType=GraphicShirt&clotheColor=Heather&graphicType=Bear&eyeType=Surprised&eyebrowType=Default&mouthType=Sad&skinColor=Pale"
-        name="Obi-Wan Kenobi"
-        time="12:45"
-        content="You were the Chosen One!"
-        isResponse
-        footer="Delivered"
-      />
-      <Bubble
-        avatar="https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Kurt&facialHairType=BeardLight&facialHairColor=Blonde&clotheType=CollarSweater&clotheColor=PastelRed&eyeType=Cry&eyebrowType=UpDownNatural&mouthType=Disbelief&skinColor=Yellow"
-        name="Anakin"
-        content="I hate you!"
-        time="12:46"
-        footer="Seen at 12:46"
-      />
-      <div className={style.sendMessage}>
+    <div className="flex flex-col" style={{ height: 500 }}>
+      <div className="flex flex-1 overflow-auto flex-col-reverse">
+        {messages.map((message) => (
+          <Bubble
+            avatar={message.avatar}
+            name={message.name}
+            time={message.time}
+            content={message.content}
+            isResponse={message.isResponse}
+            footer={message.footer}
+          />
+        ))}
+      </div>
+      <div className={style.sendMessage + ' flex-0 '}>
         <input
           type="text"
           placeholder="Type here"
           className="input input-bordered w-full"
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              onChatSubmit(e.currentTarget.value);
+              e.currentTarget.value = '';
+            }
+          }}
         />
         <div className="flex justify-end">
-          <button className="btn btn-primary btn-circle">
+          <button
+            className="btn btn-primary btn-circle"
+            onClick={() => {
+              const input = document.querySelector(
+                'input.input-bordered'
+              ) as HTMLInputElement;
+              onChatSubmit(input.value);
+              input.value = '';
+            }}
+          >
             <AiOutlineSend className="text-2xl" />
           </button>
         </div>
